9546ce77170d48e8e7d1a4b3c73f0c4ef39ba011
Noepiga <noemi.piga@nhs.net>
2026-04-17 16:22:02 +0100

    change order or mkdir folders

--- src/script.sh
+++ src/script.sh
@@ -6,10 +6,10 @@ dx-download-all-inputs # download inputs from json
 
 mkdir /home/dnanexus/fastqs
 mkdir /home/dnanexus/genomeDir
-mkdir /home/dnanexus/out/output_bam_alignedtranscriptome
-mkdir /home/dnanexus/out/output_bam_bai_alignedtranscriptome
 mkdir -p /home/dnanexus/out/output_bam
 mkdir /home/dnanexus/out/output_bam_bai
+mkdir /home/dnanexus/out/output_bam_alignedtranscriptome
+mkdir /home/dnanexus/out/output_bam_bai_alignedtranscriptome
 mkdir /home/dnanexus/out/chimeric_junctions
 mkdir /home/dnanexus/out/splice_junctions
 mkdir /home/dnanexus/out/logs
